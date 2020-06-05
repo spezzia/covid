ccff396deac64d5a5e0ca73534b4d85d79a4496c
alfonsogalguera <57268703+alfonsogalguera@users.noreply.github.com>
2020-06-05 00:53:12 -0500

    cambios
    cambios

--- puebla.js
+++ puebla.js
@@ -1,30 +1,11 @@
 'use strict'
-
-google.load('visualization', '1.0', {'packages':['corechart']});
-google.setOnLoadCallback(drawSheetName1);
-
-function drawSheetName1()
-{
-    let queri = new google.visualization.Query(
-    'https://docs.google.com/spreadsheets/d/1i3NyIHxA4cse7YItA5dzrfmnMtcxSNdA639VEnYYkX0/edit#gid=0');
-    
-    queri.setQuery("select A, B, C, D,");
-
-    queri.send(handleSampleDataQueryResponseGral1);
-}
-
-function handleSampleDataQueryResponseGral1(response) {
-    if (response.isError()) {
-    alert('Error in query: ' + response.getMessage() + ' ' + response.getDetailedMessage());
-    return;}
-
-    let  data = response.getDataTable();
-
-    let opt = {
-        width: '100%',
-      height: '900px'
-    };
-    let chartMain = new google.visualization.LineChart(document.getElementById('chartpueblacontagios'));
-    chartMain.draw(data, opt);   
-}
-
+function drawTable() {
+    var data = new google.visualization.DataTable('https://docs.google.com/spreadsheets/d/1i3NyIHxA4cse7YItA5dzrfmnMtcxSNdA639VEnYYkX0/edit#gid=0');
+    var table = new google.visualization.Table(document.getElementById('table_div'));
+    table.draw(data, {showRowNumber: true, width: '100%', height: '100%'});
+  
+    google.visualization.events.addListener(table, 'select', function() {
+      var row = table.getSelection()[0].row;
+      alert('You selected ' + data.getValue(row, 0));
+    });
+  }
