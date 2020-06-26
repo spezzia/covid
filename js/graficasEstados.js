fe2b842550c714535b13843fbf4f0a46b82892fd
spezzia <50023671+spezzia@users.noreply.github.com>
2020-06-26 14:16:38 -0500

    Ascendente y Descendente

--- js/graficasEstados.js
+++ js/graficasEstados.js
@@ -3,7 +3,9 @@
 google.load('visualization', '1.0', {'packages':['corechart', 'table']});
 //google.load('visualization', '1.0', {'packages':['table']});
 
-
+var estados_republica = ['Aguascalientes','Baja California','Baja California Sur','Campeche','Coahuila','Colima','Chiapas','Chihuahua','CDMX','Durango','Guanajuato','Guerrero','Hidalgo','Jalisco','Estado de México','Michoacán','Morelos','Nayarit','Nuevo León','Oaxaca','Puebla','Querétaro','Quintana Roo','San Luis Potosí','Sinaloa','Sonora','Tabasco','Tamaulipas','Tlaxcala','Veracruz','Yucatán','Zacatecas'];
+var datos_estados = new Array();
+var contador = 0;
 google.setOnLoadCallback(drawSheetName1);
 
 function drawSheetName1()
@@ -22,13 +24,18 @@ function handleSampleDataQueryResponseGral1(response) {
     return;}
 
     let  data = response.getDataTable();
-
+    //console.log(data);
     let opt = {
         width: '100%',
       height: '400px'
     };
     let chartMain = new google.visualization.LineChart(document.getElementById('chartMain'));
     chartMain.draw(data, opt);   
+    //for(var i = 0; i < 33 ; i++)
+    {
+        DatosEstados(estados_republica[0]);
+    }
+    console.log(datos_estados)
 }
 
 google.setOnLoadCallback(drawSheetName);
@@ -49,6 +56,7 @@ function handleSampleDataQueryResponse(response) {
     var data = response.getDataTable();
     data.Kf[1].label = "Contagios";
     data.Kf[2].label = "Decesos";
+
     //console.log(data.Kf[1].label);
 
     var options = {
@@ -61,6 +69,29 @@ function handleSampleDataQueryResponse(response) {
 
 google.setOnLoadCallback(drawSheetNameT);
 var cueritoTabla;
+function casosconfirmadosestados(cueritoTabla) {
+    var query = new google.visualization.Query(
+    'https://docs.google.com/spreadsheets/d/1t8yE6uEjGOxPyVljYUkDrPv_d7sVdCaEMg0L-9UM4D0/edit#gid=0');
+    query.setQuery(cueritoTabla);
+    return query.send(datosconfirmados);
+  }
+
+  function datosconfirmados(response) {
+    if (response.isError()) {
+      alert('Error in query: ' + response.getMessage() + ' ' + response.getDetailedMessage());
+      return;
+    }
+    var data = response.getDataTable();
+    datos_estados.push([data.hg[0].c[0].v,estados_republica[contador]]);
+    contador = contador + 1; 
+    if(contador < 33)
+    {
+        DatosEstados(estados_republica[contador]);
+    }
+  }
+
+
+
 function drawSheetNameT(cueritoTabla) {
     var query = new google.visualization.Query(
     'https://docs.google.com/spreadsheets/d/1t8yE6uEjGOxPyVljYUkDrPv_d7sVdCaEMg0L-9UM4D0/edit#gid=0');
@@ -136,7 +167,6 @@ estados.addEventListener('change', (event)=>{
     var est = `${event.target.value}`;
     buscarEstado(est);
 });
-
 function buscarEstado(est){
     let state = est;
     switch(state){
@@ -170,7 +200,7 @@ function buscarEstado(est){
         drawSheetName(cuerito);
         drawSheetNameT(cueritoTabla);
         break;
-    case 'Colima':
+    case 'Colima':  
         cuerito = "select A, L, M";
         cueritoTabla = "select P,Q,R";
         drawSheetName(cuerito);
@@ -335,4 +365,179 @@ function buscarEstado(est){
     }
 }
 
+
+function DatosEstados(est){
+    let state = est;
+    switch(state){
+    case 'Aguascalientes':
+        cueritoTabla = "select C";
+        casosconfirmadosestados(cueritoTabla);
+        break;
+    case 'Baja California':
+        cueritoTabla = "select F";
+        casosconfirmadosestados(cueritoTabla);
+        break;
+    case 'Baja California Sur':
+        cueritoTabla = "select I";
+        casosconfirmadosestados(cueritoTabla);
+        break;    
+    case 'Campeche':
+        cueritoTabla = "select L";
+        return casosconfirmadosestados(cueritoTabla);
+        break;
+    case 'Coahuila':
+        cueritoTabla = "select O";
+        return casosconfirmadosestados(cueritoTabla);
+        break;
+    case 'Colima':  
+        cueritoTabla = "select R";
+        return casosconfirmadosestados(cueritoTabla);
+        break;
+    case 'Chiapas':
+        cueritoTabla = "select U";
+        return casosconfirmadosestados(cueritoTabla);
+        break;
+    case 'Chihuahua':
+        cueritoTabla = "select X";
+        return casosconfirmadosestados(cueritoTabla);
+        break;
+    case 'CDMX':
+        cueritoTabla = "select AA";
+        return casosconfirmadosestados(cueritoTabla);
+        break;
+    case 'Durango':
+        cueritoTabla = "select AD";
+        return casosconfirmadosestados(cueritoTabla);
+        break;
+    case 'Guanajuato':
+        cueritoTabla = "select AG";
+        return casosconfirmadosestados(cueritoTabla);
+        break;
+    case 'Guerrero':
+        cueritoTabla = "select AJ";
+        return casosconfirmadosestados(cueritoTabla);
+        break;
+    case 'Hidalgo':
+        cueritoTabla = "select AM";
+        return casosconfirmadosestados(cueritoTabla);
+        break;
+    case 'Jalisco':
+        cueritoTabla = "select AP";
+        return casosconfirmadosestados(cueritoTabla);
+        break;
+    case 'Estado de México':
+        cueritoTabla = "select AS";
+        return casosconfirmadosestados(cueritoTabla);
+        break;
+    case 'Michoacán':
+        cueritoTabla = "select AV";
+        return casosconfirmadosestados(cueritoTabla);
+        break;
+    case 'Morelos':
+        cueritoTabla = "select AY";
+        return casosconfirmadosestados(cueritoTabla);
+        break;
+    case 'Nayarit':
+        cueritoTabla = "select BB";
+        return casosconfirmadosestados(cueritoTabla);
+        break;
+    case 'Nuevo León':
+        cueritoTabla = "select BE";
+        return casosconfirmadosestados(cueritoTabla);
+        break;                                 
+    case 'Oaxaca':
+        cueritoTabla = "select BH";
+        return casosconfirmadosestados(cueritoTabla);
+        break;
+    case 'Puebla':
+        cueritoTabla = "select BK";
+        return casosconfirmadosestados(cueritoTabla);
+        break;
+    case 'Querétaro':
+        cueritoTabla = "select BN";
+        return casosconfirmadosestados(cueritoTabla);
+        break;
+    case 'Quintana Roo':
+        
+        cueritoTabla = "select BQ";
+        return casosconfirmadosestados(cueritoTabla);
+        break;
+    case 'San Luis Potosí':
+        cueritoTabla = "select BT";
+        return casosconfirmadosestados(cueritoTabla);
+        break;
+    case 'Sinaloa':
+        cueritoTabla = "select BW";
+        return casosconfirmadosestados(cueritoTabla);
+        break;
+    case 'Sonora':
+        cueritoTabla = "select BZ";
+        return casosconfirmadosestados(cueritoTabla);
+        break;
+    case 'Tabasco':
+        cueritoTabla = "select CC";
+        return casosconfirmadosestados(cueritoTabla);
+        break;
+    case 'Tamaulipas':
+        cueritoTabla = "select CF";
+        return casosconfirmadosestados(cueritoTabla);
+        break;
+    case 'Tlaxcala':
+        cueritoTabla = "select CI";
+        return casosconfirmadosestados(cueritoTabla);
+        break;
+    case 'Veracruz':
+        cueritoTabla = "select CL";
+        return casosconfirmadosestados(cueritoTabla);
+        break;
+    case 'Yucatán':
+        cueritoTabla = "select CO";
+        return casosconfirmadosestados(cueritoTabla);
+        break;
+    case 'Zacatecas':
+        cueritoTabla = "select CR";
+        return casosconfirmadosestados(cueritoTabla);
+        break;
+    }
+}
+
+function Orden()
+{
+    var x = document.getElementById("orden").value;
+    if(x == "Ascendente")
+    {
+        datos_estados.sort(function(a, b){return a[0]-b[0]});
+        console.log(datos_estados);
+        var select = document.getElementById("estado");
+        var opciones = select.getElementsByTagName("option");
+        console.log(opciones[1]);
+        
+        for(var i = 0; i < 32 ; i++)
+        {
+            var nuevo = document.createElement("option");
+            var txt = document.createTextNode(datos_estados[i][1]);
+            nuevo.setAttribute("value",datos_estados[i][1]);
+            nuevo.appendChild(txt);
+            select.replaceChild(nuevo,opciones[i]);
+        }
+
+    }
+    if(x == "Descendente")
+    {   
+        datos_estados.sort(function(a, b){return b[0]-a[0]});
+        console.log(datos_estados);
+        var select = document.getElementById("estado");
+        var opciones = select.getElementsByTagName("option");
+        console.log(opciones[1]);
+        
+        for(var i = 0; i < 32 ; i++)
+        {
+            var nuevo = document.createElement("option");
+            var txt = document.createTextNode(datos_estados[i][1] );
+            nuevo.setAttribute("value",datos_estados[i][1]);
+            nuevo.appendChild(txt);
+            select.replaceChild(nuevo,opciones[i]);
+        }
+    }
+}
 
