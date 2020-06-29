676390d515866df739c6cead1c2e1741574d716a
spezzia <50023671+spezzia@users.noreply.github.com>
2020-06-29 09:58:59 -0500

    Ordenar

--- js/puebla.js
+++ js/puebla.js
@@ -5,6 +5,8 @@ google.setOnLoadCallback(drawSheetName);
 //google.load('visualization', '1.0', {'packages' : ['table']});
   //google.setOnLoadCallback(init);
 
+  var datos_estados = new Array();
+  var casos_totales = 0;
 
 /*Grafica*/
 function drawSheetName() {
@@ -51,23 +53,43 @@ function handleSampleDataQueryResponseT(response) {
     console.log(data);
     console.log(data.hg[0].c[1]);
     console.log(data.hg[0].c[1].v);
+
+    var div_ord = document.getElementById('orden');
+    var select = document.createElement('select');
+    select.setAttribute('class','custom-select');
+    select.setAttribute('id','ordenar');
+    select.setAttribute('name','ordenar');
+    var op = ['Ordenar','Ascendente','Descendente'];
+    for(var i = 0 ; i < op.length; i++)
+    {
+        var opt = document.createElement('option');
+        opt.setAttribute('vale',op[i]);
+        var tx = document.createTextNode(op[i]);
+        opt.appendChild(tx);
+        select.appendChild(opt);
+    }
+
+    div_ord.appendChild(select);
+
+
     
     var div = document.getElementById('tableMun');
-   /* var tablaanterior = div.firstChild;
-    div.removeChild(tablaanterior);*/
     var tabla = document.createElement('table');
     tabla.setAttribute('class','table');
     tabla.setAttribute('id','municipios');
     var Ttabla = document.createElement('thead');
     var trhead = document.createElement('tr');
+    var titulos = new Array();
     for(var i = 0; i < 3 ; i++)
     {
         var title = document.createElement('th');
         title.setAttribute('scope',"col");
        var text = document.createTextNode(data.Kf[i].label);
+       titulos.push(data.Kf[i].label);
         title.appendChild(text);
         trhead.appendChild(title);
     }
+    datos_estados.push(titulos);
     Ttabla.appendChild(trhead);
     tabla.appendChild(Ttabla);
 
@@ -77,6 +99,7 @@ function handleSampleDataQueryResponseT(response) {
     while(data.hg[i] != null)
     {
         var trhead = document.createElement('tr');
+        var fila = new Array();
         for(var e = 0; e < 3 ; e++)
         {
             var td = document.createElement('td');
@@ -87,16 +110,98 @@ function handleSampleDataQueryResponseT(response) {
             else
             {
                 var text = document.createTextNode(data.hg[i].c[e].v );
+                fila.push(data.hg[i].c[e].v );  
+            }
+            if(i == 0 && e == 2)
+            {
+                casos_totales = data.hg[i].c[e].v;
             }
             td.appendChild(text);
             trhead.appendChild(td);
         }
+        datos_estados.push(fila);
         tbody.appendChild(trhead);
         i++;
     }
     tabla.appendChild(tbody);
 
     div.appendChild(tabla);
+
+    const ordenar = document.querySelector("#ordenar");
+    ordenar.addEventListener('change', (event)=>{
+    var est = `${event.target.value}`;
+    console.log(datos_estados);
+    if(est == "Ascendente")
+    {
+        datos_estados.sort(function(a, b){return a[1]-b[1]});
+        console.log(datos_estados);
+        ordenar_tabla();
+
+    }
+    if(est == "Descendente")
+    {   
+        datos_estados.sort(function(a, b){return b[1]-a[1]});
+        console.log(datos_estados);
+        ordenar_tabla();
+    }
+    });
     /*var chart = new google.visualization.Table(document.getElementById('tabMundial'));
     chart.draw(data, { height: 250 });*/
+    function ordenar_tabla()
+    {
+      var div = document.getElementById('tableMun');
+      var tablaanterior = div.firstChild;
+      div.removeChild(tablaanterior);
+      var tabla = document.createElement('table');
+      tabla.setAttribute('class','table');
+      tabla.setAttribute('id','municipios');
+      var Ttabla = document.createElement('thead');
+      var trhead = document.createElement('tr');
+      for(var i = 0; i < 3 ; i++)
+      {
+          var title = document.createElement('th');
+          title.setAttribute('scope',"col");
+          var text = document.createTextNode(datos_estados[0][i]);
+          title.appendChild(text);
+          trhead.appendChild(title);
+      }
+      Ttabla.appendChild(trhead);
+      tabla.appendChild(Ttabla);
+  
+      var tbody = document.createElement('tbody');
+  
+      var i = 1;
+      while(datos_estados[i] != null)
+      {
+          var trhead = document.createElement('tr');
+          for(var e = 0; e < 3 ; e++)
+          {
+              var td = document.createElement('td');
+              if(datos_estados[i][e] == null)
+              {
+                  var text = document.createTextNode('');
+              }
+              else
+              {
+                  var text = document.createTextNode(datos_estados[i][e]);
+              }
+              if(i == 1 && e == 2)
+              {
+                  var text =  document.createTextNode(casos_totales);
+              }
+              if(e == 2 && datos_estados[i][e] != null )
+              {
+                  var text = document.createTextNode('');   
+              }
+              td.appendChild(text);
+              trhead.appendChild(td);
+          }
+          tbody.appendChild(trhead);
+          i++;
+      }
+      tabla.appendChild(tbody);
+  
+      div.appendChild(tabla);
+    }
 }
+
